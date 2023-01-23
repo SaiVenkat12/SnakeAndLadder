9c11660b7caa33d27b782357f51e2a0fcf4537cc
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-23 21:48:17 +0530

    [SaiVenkat] Added Options snake,ladder,NoGame

--- SnakeAndLadder/Game.cs
+++ SnakeAndLadder/Game.cs
@@ -10,11 +10,39 @@ namespace SnakeAndLadder
     {
         public static void GamePlay()
         {
+            const int snake = 0;
+            const int ladder = 1;
+            const int NoGame= 2;
             int position = 0;
             Random random= new Random();
             int diceRoll=random.Next(1, 7);
-            position = diceRoll ;
-            Console.WriteLine("Player 1 position : " + position);
+            Console.WriteLine("Dice Roll= "+diceRoll);
+            int choice=random.Next(0,3);
+            
+            switch(choice)
+            {
+                case snake:
+                    position -= diceRoll;
+                    Console.WriteLine("Snake");
+                    if(position<0)
+                    {
+                        Console.WriteLine("Player 1 position : 0");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Player 1 position : " + position);
+                    }
+                    break;
+                case ladder:
+                    position += diceRoll;
+                    Console.WriteLine("Ladder");
+                    Console.WriteLine("Player 1 position : " + position);
+                    break;
+                case NoGame:
+                    Console.WriteLine("No Game");
+                    Console.WriteLine("Player 1 position : " + position);
+                    break;
+            }
         }
     }
 }
