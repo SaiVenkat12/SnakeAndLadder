3584d108584fffaad82c85d523a8c56711b4daf1
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-23 22:07:41 +0530

    [SaiVenkat] Added Player reaches 100 then Player Wins

--- SnakeAndLadder/Game.cs
+++ SnakeAndLadder/Game.cs
@@ -15,33 +15,47 @@ namespace SnakeAndLadder
             const int NoGame= 2;
             int position = 0;
             Random random= new Random();
-            int diceRoll=random.Next(1, 7);
-            Console.WriteLine("Dice Roll= "+diceRoll);
-            int choice=random.Next(0,3);
             
-            switch(choice)
+            while(position<100)
             {
-                case snake:
-                    position -= diceRoll;
-                    Console.WriteLine("Snake");
-                    if(position<0)
-                    {
-                        Console.WriteLine("Player 1 position : 0");
-                    }
-                    else
-                    {
+                int diceRoll = random.Next(1, 7);
+                //Console.WriteLine("Dice Roll= " + diceRoll);
+                int choice = random.Next(0, 3);
+                switch (choice)
+                {
+                    case snake:
+                        position -= diceRoll;
+                        Console.WriteLine("Snake");
+                        if (position < 0)
+                        {
+                            position = 0;
+                            Console.WriteLine("Player 1 position : " + position);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Player 1 position : " + position);
+                        }
+                        break;
+                    case ladder:
+                        position += diceRoll;
+                        Console.WriteLine("Ladder");
+                        if(position>=100)
+                        {
+                            position = 100;
+                            Console.WriteLine("Player 1 position : " + position);
+                            Console.WriteLine("player1 Wins");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Player 1 position : " + position);
+                        }
+                        break;
+                    case NoGame:
+                        Console.WriteLine("No Game");
                         Console.WriteLine("Player 1 position : " + position);
-                    }
-                    break;
-                case ladder:
-                    position += diceRoll;
-                    Console.WriteLine("Ladder");
-                    Console.WriteLine("Player 1 position : " + position);
-                    break;
-                case NoGame:
-                    Console.WriteLine("No Game");
-                    Console.WriteLine("Player 1 position : " + position);
-                    break;
+                        break;
+                }
+            
             }
         }
     }
